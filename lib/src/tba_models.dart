25951714960ef78ad2b22149d0e4d00c348e09a9
Project516 <138796702+Project516@users.noreply.github.com>
2026-08-21 17:05:48 -0500

    Format tba_models.dart

--- lib/src/tba_models.dart
+++ lib/src/tba_models.dart
@@ -533,8 +533,7 @@ class TbaTeamRanking {
 
   /// The numeric team number parsed from [teamKey], or 0 when the key does
   /// not follow the `frc{n}` convention. Mirrors [TbaTeam.teamNumber].
-  int get teamNumber =>
-      int.tryParse(teamKey.replaceFirst('frc', '')) ?? 0;
+  int get teamNumber => int.tryParse(teamKey.replaceFirst('frc', '')) ?? 0;
 }
 
 /// `GET /event/{eventKey}/alliances`: playoff alliances as picked (#11).
